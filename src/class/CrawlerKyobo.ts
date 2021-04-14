17a38e1ff08e47c35617076d2f5c2421b876698f
최영훈 <c_yh0327@naver.com>
2021-04-14 21:08:59 +0900

    [fix] networkIdle0

---
+++ src/class/CrawlerKyobo.ts
@@ -0,0 +1,22 @@
+import * as _ from "lodash";
+import { kyoboBook } from "../modules/scrapper";
+/**
+ * Singleton Class
+ */
+class Crawler {
+  private static instance: Crawler;
+
+  private constructor() {}
+
+  public static getInstance(): Crawler {
+    if (_.isNil(this.instance)) this.instance = new Crawler();
+    return this.instance;
+  }
+
+  public async crawling(title: string) {
+    const result = await kyoboBook(title);
+    return result;
+  }
+}
+
+export default Crawler.getInstance();
